9d3fb389974613d68705a43499838eb8301f774d
sid dev <semple2266@gmail.com>
2025-06-23 05:16:07 +0000

    use this png as a my favicon or logo of the app

--- src/components/icons/LogoIcon.tsx
+++ src/components/icons/LogoIcon.tsx
@@ -3,17 +3,27 @@ import type { SVGProps } from 'react';
 export function LogoIcon(props: SVGProps<SVGSVGElement>) {
   return (
     <svg
-      xmlns="http://www.w3.org/2000/svg"
-      viewBox="0 0 24 24"
-      fill="none"
-      stroke="currentColor"
-      strokeWidth="2"
-      strokeLinecap="round"
-      strokeLinejoin="round"
-      {...props}
+        xmlns="http://www.w3.org/2000/svg"
+        viewBox="0 0 32 32"
+        fill="none"
+        {...props}
     >
-      <path d="M12 22c5.523 0 10-4.477 10-10S17.523 2 12 2 2 6.477 2 12s4.477 10 10 10z" />
-      <path d="M9.5 7L14.5 12L9.5 17" />
+        <defs>
+            <linearGradient id="logoGradient" x1="1" y1="0" x2="0" y2="1">
+                <stop offset="0%" stopColor="#d946ef" />
+                <stop offset="100%" stopColor="#4f46e5" />
+            </linearGradient>
+        </defs>
+        <g fill="currentColor">
+            <path
+                d="M14.5 5a1.5 1.5 0 0 1 3 0v9.5h9.5a1.5 1.5 0 0 1 0 3h-9.5v9.5a1.5 1.5 0 0 1-3 0v-9.5H5a1.5 1.5 0 0 1 0-3h9.5V5z"
+                transform="rotate(45 16 16)"
+            />
+            <path
+                d="M14.5 5a1.5 1.5 0 0 1 3 0v9.5h9.5a1.5 1.5 0 0 1 0 3h-9.5v9.5a1.5 1.5 0 0 1-3 0v-9.5H5a1.5 1.5 0 0 1 0-3h9.5V5z"
+            />
+        </g>
+        <path d="M18 0H32V14H28V4H18V0Z" fill="url(#logoGradient)" />
     </svg>
   );
 }
